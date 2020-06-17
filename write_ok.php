b09ae01846d5158d22344d8ec05a75bfd187b695
yeongjun0807 <62930087+yeongjun0807@users.noreply.github.com>
2020-06-17 14:55:36 +0900

    Update write_ok.php

--- write_ok.php
+++ write_ok.php
@@ -1,29 +1,18 @@
 <?php
-$captcha = $_POST['g-recaptcha'];
-$secretKey = '6LeyuKUZAAAAADsbKtnn5cACbmKbYI3UAONy6p42'; // 위에서 발급 받은 "비밀 키"를 넣어줍니다.
-$ip = $_SERVER['REMOTE_ADDR']; // 옵션값으로 안 넣어도 됩니다.
-
-$data = array(
-  'secret' => $secretKey,
-  'response' => $captcha,
-  'remoteip' => $ip  // ip를 안 넣을거면 여기서도 빼줘야죠
-);
-
-$url = "https://www.google.com/recaptcha/api/siteverify";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-$response = curl_exec($ch);
-curl_close($ch);
-
-$responseKeys = json_decode($response, true);
-
-if ($responseKeys["success"]) {
-  alert('y')
-  // 스팸 검사가 통과 했을 때의 처리
-} else {
-  alert('n')
-  // 스팸 검사가 실패 했을 때의 처리
-}
+if(isset($_POST['g-recaptcha-response'])){
+          $captcha=$_POST['g-recaptcha-response'];
+        }
+        if(!$captcha){
+          echo '<h2리캡차를 확인하세요</h2>';
+          exit;
+        }
+        $secretKey = "6LeyuKUZAAAAADsbKtnn5cACbmKbYI3UAONy6p42";
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+        $responseKeys = json_decode($response,true);
+        if(intval($responseKeys["success"]) !== 1) {
+          echo '<h2>너 스패머지? 나가라</h2>';
+        } else {
+          echo '<h2>고맙습니다.</h2>';
+        }
+?>
