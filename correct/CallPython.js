ff1c7e0a70ba6b2d8f116476834edc8b011e35bd
yeongjun0807 <62930087+yeongjun0807@users.noreply.github.com>
2020-06-17 19:36:36 +0900

    Update CallPython.js

--- correct/CallPython.js
+++ correct/CallPython.js
@@ -1,4 +1,13 @@
-var Button = document.getElementById('Button');
+/*var Button = document.getElementById('Button');
+var {PythonShell} = require('python-shell');
 Button.addEventListener('click', function(){
     arrs = openSomehowPythonInterpreter("~/crawling.py", "correct()");
-})
+})*/
+$.ajax({
+  type: "POST",
+  url: "~/crawling.py",
+  data: { param: text}
+}).done(function( o ) {
+   alert('fd')
+    // do something
+});
