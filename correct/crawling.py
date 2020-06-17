9d5b63692aa783ccd2e2cfdbd88dfe972c8d4cad
yeongjun0807 <62930087+yeongjun0807@users.noreply.github.com>
2020-06-17 19:28:08 +0900

    Add files via upload

---
+++ correct/crawling.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+
+def correct():
+    try:
+        driver = webdriver.Chrome('./driver/chromedriver.exe')
+
+    except:
+        driver = webdriver.Chrome('./driver/chromedriver')
+
+    driver.maximize_window()
+
+    site = ['https://m.blog.naver.com/shining0721', 'https://m.blog.naver.com/desbey7']
+    Title = []
+    Image = []
+
+    for j in range(0, 2):
+        driver.get(site[j])
+
+        for i in range(1, 6):
+            try:
+                Title.append(driver.find_element_by_xpath(f'/html/body/ui-view/bg-nsc/div[9]/div[4]/div/div/div[1]/ul/li[{i}]/div/a/div[2]/strong/span').text)
+                LoadImage = driver.find_elements_by_xpath(f'/html/body/ui-view/bg-nsc/div[9]/div[4]/div/div/div[1]/ul/li[{i}]/div/a/div[1]')
+                for e in LoadImage:
+                    Image.append(e.get_attribute("bg-lazy-img"))
+
+            except:
+                Title.append(driver.find_element_by_xpath(f'/html/body/ui-view/bg-nsc/div[9]/div[4]/div/div/div[1]/ul/li[{i}]/div/a/div/strong/span').text)
+                Image.append('https://i.imgur.com/5QhPOLP.png')
+
+        driver.back()
+
+    print(Title)
+    print(Image)
+
+    driver.quit()
