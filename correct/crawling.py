2593d489562f7e31fb9be5aac82dbe65e00da8f5
yeongjun0807 <62930087+yeongjun0807@users.noreply.github.com>
2020-06-18 20:03:36 +0900

    Update crawling.py

--- correct/crawling.py
+++ correct/crawling.py
@@ -40,3 +40,6 @@ def correct():
     
     with open('data.json', 'w', encoding = "utf-8") as file:
         json.dump(data, file, ensure_ascii = False, indent = "\t")
+        
+    a = open('data.txt', 'w')
+    a.close()
